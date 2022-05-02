3f321debbd819e8a5d568fc8042b801319b8433a
Sunil Behera <sunil.6318ie@gmail.com>
2022-05-02 17:01:39 +0530

    add images inside src, plus made progress in user profile card

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import image from './images/image-jeremy.png';
 
 function App() {
   return (
-    <div className="container">
-      Hello Welcome to Time Tracking Dashboard
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
+    <section className="container">
+      <main className='container__content'>
+        <div className='container_content-user'>
+          <div className='user-head'>
+            <img className='profile' src={image} alt='profile' />
+            <p>Report for</p>
+            <h2 className='profile-name'>
+            Jeremy Robson
+            </h2>
+          </div>
+        </div>
+        <div className='container_content-activity'>
+          <ul>
+            <li>Work</li>
+            <li>Exercise</li>
+          </ul>
+        </div>
+      </main>
+    </section>
   );
 }
 
