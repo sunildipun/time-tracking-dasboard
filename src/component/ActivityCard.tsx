e45c94febcc97492f5195757f4fd677943377506
Sunil Behera <sunil.6318ie@gmail.com>
2022-05-04 20:29:44 +0530

    added color to cards and added icon self

--- src/component/ActivityCard.tsx
+++ src/component/ActivityCard.tsx
@@ -1,13 +1,45 @@
 import React from "react";
 import './ActivityCard.css';
 
+import {backgroundColor} from './../data.js';
+
+import Exercise from './../images/icon-exercise.svg';
+import Work from './../images/icon-work.svg';
+import Play from './../images/icon-play.svg';
+import Social from './../images/icon-social.svg';
+import Study from './../images/icon-study.svg';
+import SocialCare from './../images/icon-self-care.svg';
+
+
+
 export function ActivityCard(props: any) {
     // const activityData = props.data;
     return <ul className="activityList">
         {
             props.data.map((item: {title: string, timeframes: any}) => {
-                return <li className="cards" key={item.title}>{item.title}</li>
+                return <li className="cards" style={{backgroundColor:  backgroundColor[item.title as keyof typeof backgroundColor] }}  key={item.title}>
+                    {(() => {
+                        switch (item.title) {
+                        case 'Exercise':
+                            return <img src={Exercise} alt="exercise" />;
+                        case 'Play':
+                            return <img src={Play} alt="play" />;
+                        case 'Work':
+                            return <img src={Work} alt="work" />;
+                        case 'Social':
+                            return <img src={Social} alt="social"/>;
+                        case 'Study':
+                            return <img src={Study} alt="study"/>;
+                        case 'Self Care':
+                            return <img src={SocialCare} alt="self care" />
+                        default:
+                            return null;
+                        }
+                    })()}
+                    {item.title}
+                </li>
             })
         }
     </ul>
+
 }
--- src/data.js
+++ src/data.js
@@ -102,3 +102,13 @@ export const activityData = [
     }
   }
 ]
+
+export const backgroundColor = {
+  'user': `hsl(246, 80%, 60%)`,
+  'Work':  `hsl(15, 100%, 70%)`,
+  'Play':  `hsl(195, 74%, 62%)`,
+  'Study': `hsl(348, 100%, 68%)`,
+  'Exercise':  `hsl(145, 58%, 55%)`,
+  'Social':  `hsl(264, 64%, 52%)`,
+  'Self Care': `hsl(43, 84%, 65%)`,
+}
